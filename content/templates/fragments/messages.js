2b4cbb137a5746bc667a6c7f6c99394f4777d588
Alecco <info@alejosanchez.com>
2011-11-13 13:36:53 -0300

    Fix JS string concatenation

--- content/templates/fragments/messages.js
+++ content/templates/fragments/messages.js
@@ -26,12 +26,27 @@ function getMensaje() {
 }
 function getVisualizaMensaje() {
     // fix mgoldsman 20110831
-    document.getElementById('msjMax').innerHTML = '<div class="panelMsjTit">Tenes&nbsp;<span id="totalMsg"></span> mensajes por leer &gt;</div>'
-            + '<div class="panelMsjTxt">'
-            + '<a href="javascript:mostrarDiv(\'msjMin\'); javascript:mostrarDiv(\'msjMax\');javascript:setVisualizaMensaje(false)" class="panelMsjCerrar">cerrar</a>'
-            + '<span id="pagMsg"></span> <span id="textoMsgActual"></span> <div><a href="javascript:nada()" class="pnlMsgComandos" id="msgAnterior">&lt;Anterior</a> | <a href="javascript:nada()" class="pnlMsgComandos" id="msgSiguiente">Siguiente&gt;</a> | <a href="javascript:nada()" class="pnlMsgComandos" id="msgLeido">No volver a mostrar este mensaje</a></div>'
+    document.getElementById('msjMax').innerHTML = \
+            + '<div class="panelMsjTit">Tenes&nbsp;<span id="totalMsg">' \
+            + '</span> mensajes por leer &gt;</div>' \
+            + '<div class="panelMsjTxt">' \
+            + '<a href="javascript:mostrarDiv(\'msjMin\'); ' \
+            + 'javascript:mostrarDiv(\'msjMax\'); ' \
+            + 'javascript:setVisualizaMensaje(false);" ' \
+            + 'class="panelMsjCerrar">cerrar</a>' \
+            + '<span id="pagMsg"></span> <span id="textoMsgActual"></span> ' \
+            + '<div><a href="javascript:nada()" class="pnlMsgComandos" ' \
+            + 'id="msgAnterior">&lt;Anterior</a> | ' \
+            + '<a href="javascript:nada()" class="pnlMsgComandos" ' \
+            + 'id="msgSiguiente">Siguiente&gt;</a> | ' \
+            + '<a href="javascript:nada()" class="pnlMsgComandos" ' \
+            + 'id="msgLeido">No volver a mostrar este mensaje</a></div>'
             + '</div>';
-    document.getElementById('msjMin').innerHTML = '<div class="panelMsjTxt"><a href="javascript:mostrarDiv(\'msjMin\'); javascript:mostrarDiv(\'msjMax\');javascript:setVisualizaMensaje(true)" class="panelMsjAbrir">abrir panel y ver mensajes</a>   </div>';
+    document.getElementById('msjMin').innerHTML = \
+            + '<div class="panelMsjTxt"><a href="javascript:' \
+            + 'mostrarDiv(\'msjMin\'); mostrarDiv(\'msjMax\'); ' \
+            + 'setVisualizaMensaje(true)" ' \
+            + 'class="panelMsjAbrir">abrir panel y ver mensajes</a> </div>';
     getMensaje();
 }
 function setMensajeUsuario(indice) {
