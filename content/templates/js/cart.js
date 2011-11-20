a13d4091d7251fd0e209f66e08ddd5ded8eef27f
Alecco <info@alejosanchez.com>
2011-11-20 11:27:12 -0300

    Add formatCurrency missing function (needs fixing)

--- content/templates/js/cart.js
+++ content/templates/js/cart.js
@@ -75,3 +75,20 @@ function carrito_SetCarrito(objCarrito) {
 function agregarProducto(idArticulo) {
 	carrito_AgregarArticulo(idArticulo);
 }
+
+function formatCurrency(num) {
+    num = num.toString().replace(/\$|\,/g,'');
+    if (isNaN(num))
+      num = 0;
+    var signo = (num == (num = Math.abs(num)));
+    num = Math.floor(num * 100 + 0.50000000001);
+    centavos = num % 100;
+    num = Math.floor(num / 100).toString();
+    if (centavos < 10)
+      centavos = '0' + centavos;
+    for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+
+    return (((signo) ? '' : '-') + num + '.' + centavos);
+}
+
