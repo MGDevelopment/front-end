918dddf0cd9201004beb261b436b70c4037395c1
Alecco <info@alejosanchez.com>
2011-12-03 12:16:35 -0300

    Section showcase dynamic data (not working yet)

---
+++ content/templates/subject/showcase.js
@@ -0,0 +1,25 @@
+{%- import 'macros/slider.html' as render -%}
+/**
+* section-showcase.js
+*/
+var dataShowCase = {};
+
+{%- for key in d['ShowCase'] -%}
+	{%- for subKey in d[key] -%}
+        {%- set dataKey = key|replace("Recommended-","")|replace("Showcase-","")|lower -%}
+		{# dataShowCase['{{ subKey }}{{ key }}'] = '{{ render.renderSlider(showcase_data[key][subKey], key)|replace("\n", "") }}'; #}
+        dataShowCase['{{ key }}'] = '{{ render.renderSlider(d[key], dataKey, 6, url)}}';
+	{%- endfor %}
+{%- endfor -%}
+
+function loadShowCase() {
+	TMK.addData('showcase', dataShowCase);
+	return;
+} 
+
+var callBackDropDownShowCase = function (section) {
+	
+	return;
+}
+
+loadShowCase();
