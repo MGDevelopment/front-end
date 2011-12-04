91349e8a5dd1ed016e99ecffcba69bf70a86cbca
Jose Luis Campanello <jose.luis.campanello@gmail.com>
2011-12-04 13:32:19 -0300

    Added logging to generator, fixed typo in SUBJ datasets, regenerated SUBJ sample data

--- content/daemon.py
+++ content/daemon.py
@@ -4,6 +4,9 @@ by Jose Luis Campanello
 """
 
 import time
+import logging
+import logging.config
+import traceback
 
 import ecommerce.db.dataset
 import ecommerce.queue
@@ -15,31 +18,45 @@ import jobs
 import documents
 import work
 
+#
+# our logger
+#
+_logger = None
+
 
 ############################################
 #
 # job - generate
 #
-def jobGenerate(job):
+def jobGenerate(id, job):
     """Handle a content generation job"""
+
+    logger.info("Processing generate Job id %s", id)
 
     # find the relevant documents (for the tag, if present)
     tag  = job.get("tag", "*")
     docs = documents.relevant(tag)
+    logger.debug("Got relevant documents")
 
     # extract the lists of datasets
     datasetNames = {
         e : { docs[e][i]["dataset"] : 1 for i in range(len(docs[e])) }.keys()
         for e in docs
     }
+    logger.debug("Calculated dataset names")
 
     # get the entities
     entities = job["entities"]
+    logger.debug("Got entities")
 
     # get the cannonicals, if anything goes wrong, fail the job
     try:
         urls = tmklib.url.cannonicals(entities)
+        logger.debug("Got cannonicals")
     except Exception as ex:
+        # log an error
+        err = traceback.format_exc()
+        logger.error("Failed to get cannonicals, stack trace follows\n%s", err)
         # job in error
         return False
 
@@ -65,13 +82,19 @@ def jobGenerate(job):
     try:
         result = ecommerce.db.dataset.fetch(datasets)
     except Exception as ex:
+        # log an error
+        err = traceback.format_exc()
+        logger.error("Failed to get datasets, stack trace follows:\n%s", err)
         # job in error
         return False
     if reduce(lambda x, y: x or y[2], result, False):
+        # log an error
+        failed = "\n".join( [ str( (datasets[i][0],
+                                    datasets[i][1],
+                                    datasets[i][2],
+                                    result[i][3]) ) for i in range(len(datasets)) if result[i][2] ] )
+        logger.error("Failed to get datasets. The following datasets executed with errors:\n%s", failed)
         return False
-
-    #import pprint
-    #pprint.pprint(result, open("jlc_data.py", "w"), 4)
 
     # build handy data (key being <EntityType, EntityId, datasetName>)
     data = { }
@@ -79,7 +102,7 @@ def jobGenerate(job):
         data[datasets[d]] = result[d][3]
 
     # do the work
-    return work.do(entities, docs, cannonicals, data)
+    return work.do(id, entities, docs, cannonicals, data)
 
 
 ############################################
@@ -94,12 +117,14 @@ def terminating():
     return _terminating
 
 
-def jobExit(job):
+def jobExit(id, job):
     """Mark termination"""
 
     global _terminating
 
     _terminating = True
+
+    logger.info("Processing exit Job. Setting terminate mark")
 
     return True
 
@@ -120,18 +145,37 @@ def daemon(config, prefix, queue):
     starts trying to pull jobs from the queue.
     """
 
+    global logger
+
+    # configure logging
+    logging.basicConfig(level = logging.INFO, datefmt = '%Y-%m-%d %H:%M:%S')
+    logconf = config.getMulti(prefix, "logging")
+    if logconf is not None:
+
+        # config the logging
+        logging.config.dictConfig(logconf) 
+
+        # get a logger
+        logger = logging.getLogger(prefix)
+    else:
+        logger = logging.getLogger('')
+
+
     # get some config options
     maxrun = config.getMulti(prefix, "daemon.maxrun", 3600)
     endBy  = time.time() + maxrun
 
+    logger.info("Daemon started - maxrun %d", maxrun)
+
     # initialize the work
-    work.initialize(config, prefix)
+    work.initialize(config, prefix, logger)
 
     # set dataset preprocessing
     ecommerce.db.dataset.setPreProcess(preprocess.preProcess)
 
     # start looping
     now  = time.time()
+    logger.info("Daemon will end by %s", endBy)
     while not terminating() and now < endBy:
 
         # try to fetch a job from the queue (non blocking)
@@ -147,24 +191,35 @@ def daemon(config, prefix, queue):
             # decode the job
             job = jobs.decode(item.content)
 
+            logger.info("Got job %s, job type %s", item.id, job.get("type", "unknown"))
+
             # get the job type and dispatch it
             jobType = job.get("type")
             if jobType not in jobTypes:
                 handled = False
             else:
-                handled = jobTypes[jobType](job)
+                handled = jobTypes[jobType](item.id, job)
 
             # mark as done or error
             if handled:
+                logger.info("Job %s processed ok", item.id)
                 queue.done(item)
             else:
+                logger.info("Job %s processed with ERROR", item.id)
                 queue.error(item)
+        else:
+            logger.debug("Job Queue empty")
 
-        # sleep for 1 second
-        time.sleep(1)
+        # sleep for 1 second (if queue is empty)
+        if queue.isEmpty():
+            logger.debug("Waiting 1 second...")
+            time.sleep(1)
 
         # get current time
         now = time.time()
+
+    logger.info("Daemon terminating - reason: %s",
+                "command" if terminating() else "maxrun reached")
 
     # deinitialize the work
     work.deinitialize()
