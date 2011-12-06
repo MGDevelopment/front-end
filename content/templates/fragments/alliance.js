8709430e5fcbdc5b546697c0af177a38f2b1c938
Mariano Goldsman <mgoldsman@gmail.com>
2011-12-06 12:23:04 -0300

    funcionalidad de alianzas contra el JRun

---
+++ content/templates/fragments/alliance.js
@@ -0,0 +1,27 @@
+/**
+ * alliance.js
+ */
+
+
+function gup( varname ) {
+    varname = varname.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    var regexS = "[\\?&]"+varname+"=([^&#]*)";
+    var regex = new RegExp( regexS );
+    var results = regex.exec( window.location.href );
+    if( results == null ) return ""; else return results[1];
+}
+
+function processAlliance(){
+	if (gup("ID_ALIANZA") != "") {
+    	$.ajax({
+    		type: "GET",
+    		url: "/alianzas/alianzas.jsp?idArticulo={{ d.EntityId }}&ID_ALIANZA=" + gup('ID_ALIANZA') + "&ID_SECCION={{ d.Categoria_Seccion}}",
+    		dataType: "xml",
+    		success: function() {
+    		}
+    	});
+	}
+	return;
+}
+
+processAlliance();
